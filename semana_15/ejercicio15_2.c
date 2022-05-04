66229250343bc32e89d1818c1dce068c80076c67
saulrazo <saulrazom32@gmail.com>
2022-05-03 20:52:03 -0500

    1

--- semana_15/ejercicio15_2.c
+++ semana_15/ejercicio15_2.c
@@ -19,18 +19,18 @@ int main(void){
 
 void reverseString(char s1[])
 {   
-    int cont,lenght = strlen(s1);
+    int cont,length = strlen(s1), halfLength=length/2;
     char aux;
     
      /*Determina las repeticiones del ciclo de acuerdo a 
      cuántos cambios de posición se realizan, los cuales se
     relacionan con la mitad de la longitude de la cadena*/
 
-    for (cont = 0; cont < strlen(s1) / 2; cont++)
+    for (cont = 0; cont < halfLength; cont++)
     {
         aux = s1[cont]; //Guarda el valor a intercambiar en una variable auxiliar
-        s1[cont] = s1[lenght - cont - 1]; //Realiza el cambio en cuyo caso de la primera con la última, segunda con penúltima...
-        s1[lenght - cont - 1] = aux; //Asigna en cuyo caso al último espacio el valor del auxiliar que correspondía al otro extremo de la palabra.
+        s1[cont] = s1[length - cont - 1]; //Realiza el cambio en cuyo caso de la primera con la última, segunda con penúltima...
+        s1[length - cont - 1] = aux; //Asigna en cuyo caso al último espacio el valor del auxiliar que correspondía al otro extremo de la palabra.
     }
 }
 
